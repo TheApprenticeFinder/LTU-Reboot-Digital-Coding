47e1cafc9c32f153d95d0182829cddf6f9d7132a
TheApprenticeFinder <adrian.bird@theapprenticefinder.com>
2021-02-11 20:10:03 +0000

    Update task-3.1.2.js

---
+++ module-3/lesson-1/scripts/task-3.1.2.js
@@ -0,0 +1,13 @@
+/*
+
+This is a 
+multiple line
+comment
+
+*/
+
+// this is a single line comment
+
+alert("Hello from the alert box"); // This pops up with an alert message
+console.log("Hello from the console"); // Shows a message in the console
+document.write("Hello from the document.write"); // prints a messsage to the screen
